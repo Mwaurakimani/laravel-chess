199f53d894836e33dcc7c142f315c889e8df43de
Peter Mwaura <kimmwaus@gmail.com>
2025-07-18 02:21:53 +0300

    Clean challenge creation form

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
             $this->call([
                 RoleSeeder::class,
                 PlatformSeeder::class,
-                UsersSeeder::class,
+//                UsersSeeder::class,
 //                ChallengeSeeder::class,
 //                ChessMatchesResultsSeeder::class
             ]);
